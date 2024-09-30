7db615a94339447ec295e2f8d881590af13f4ef1
cem <cemyilmaztech@gmail.com>
2024-09-30 20:39:32 +0300

    productAction

--- src/redux/action/productAction.js
+++ src/redux/action/productAction.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 // Ürün verilerini çekme aksiyonu
 export const fetchProducts = () => async (dispatch) => {
   try {
-    const response = await axios.get('https://fakestoreapi.com/products');
+    const response = await axios.get(
+      'https://fakestoreapi.com/products');
     dispatch({ type: 'FETCH_PRODUCTS_SUCCESS', payload: response.data });
    
   } catch (error) {
