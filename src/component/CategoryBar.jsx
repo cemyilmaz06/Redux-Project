912aa59a8686dd221aac04f85fe2b489c832b3a1
cem <cemyilmaztech@gmail.com>
2024-10-02 23:31:32 +0300

    productCard

--- src/component/CategoryBar.jsx
+++ src/component/CategoryBar.jsx
@@ -5,11 +5,10 @@ import { fetchCategories } from '../redux/action/productAction';
 import { filteredProducts } from '../redux/action/productAction';
 import Typography from "@mui/material/Typography";
 import { Button, Container } from '@mui/material';
-const CategoryBar = ({filteredProducts}) => {
+const CategoryBar = ({filteredProducts,}) => {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.products.categories);
  
-    
 
   useEffect(() => {
     dispatch(fetchCategories()); // Kategorileri yükle
@@ -21,7 +20,7 @@ const CategoryBar = ({filteredProducts}) => {
      
         {categories.map((category,index) => (
           
-             <button style={{color:"black",border:"none",marginLeft:"150px",display:"inline",justifyContent:"center",alignItems:"center",marginBottom:"60px", padding:"10px", borderRadius:"10px"}} key={index}  onClick={()=>dispatch(filteredProducts(category))}
+             <button style={{color:"black",border:"none",marginLeft:"150px",display:"inline",justifyContent:"center",alignItems:"center",marginBottom:"60px", padding:"10px", borderRadius:"10px"}} key={index}  
            >{category}</button>
      ))}
       </Container>
--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,67 +1,68 @@
-// src/components/ProductCard.jsx
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts } from "../redux/action/productAction";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import { IconButton } from "@mui/material";
 import { addToBasket } from "../redux/action/basketAction";
-import CategoryBar from "./CategoryBar";
-import { filteredProducts } from '../redux/action/productAction';
 import { Card, Container } from "react-bootstrap";
+
 const ProductCard = () => {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.products.products);
-  const [productData, setProductData] = useState(products)
-  const filteredProducts=(category)=>{
-  if(category === "all"){
-    setProductData(products);
-  }else{
-const selectedProducts=()=>(products.filter((product)=>product.category === category))
-setProductData(selectedProducts);
-  }
-  }
+  const [productData, setProductData] = useState(products);
+
+  // Ürünler çekildikten sonra productData'yı güncelle
+  useEffect(() => {
+    dispatch(fetchProducts()); // API'den ürünleri çek
+  }, [dispatch]);
 
   useEffect(() => {
-    dispatch(fetchProducts()); // Ürünleri yükle
-  }, [dispatch]);
+    setProductData(products); // Ürünler değiştiğinde productData'yı güncelle
+  }, [products]);
+
+  // Kategorilere göre filtreleme fonksiyonu
+  
 
   return (
     <Container className="d-flex align-item-center justify-content-center">
-    <Card  className="rounded-2 m-auto card mb-5" role="button">
-      <h2 className="text-center">Products</h2>
-      <div style={{ display: "flex" ,flexWrap:"wrap" }}>
-        {products.map((product) => (
-          <div
-            key={product.id}
-            style={{
-              border: "1px solid #ccc",
-              margin: "10px",
-              padding: "10px",
-              width:"400px",
-              height:"450px",
-              marginLeft:"20px"
-            }}
-          >
-            <img
-              src={product.image}
-              alt={product.title}
-              style={{ width: "200px",height:"250px",marginLeft:"80px"}}
-            />
-            <div className="title" >
-       
-              <h3 style={{fontSize:"20px",width:"200px"}}>{product.title}</h3>
-              <p>{product.price} $</p>
+      <Card className="rounded-2 m-auto card mb-5" role="button">
+        <h2 className="text-center">Ürünler</h2>
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {products.map((product) => ( // filteredProducts değil, productData kullanılmalı
+            <div
+              key={product.id}
+              style={{
+                border: "1px solid #ccc",
+                margin: "10px",
+                padding: "10px",
+                width: "400px",
+                height: "450px",
+                marginLeft: "20px",
+              }}
+            >
+              <img
+                src={product.image}
+                alt={product.title}
+                style={{ width: "200px", height: "250px", marginLeft: "80px" }}
+              />
+              <div className="title">
+                <h3 style={{ fontSize: "20px", width: "200px" }}>{product.title}</h3>
+                <p>{product.price} $</p>
+              </div>
+              <div className="d-flex justify-content-between">
+                {product.category}
+                <IconButton
+                  color="black"
+                  aria-label="add to shopping cart"
+                  onClick={() => dispatch(addToBasket(product))}
+                >
+                  <AddShoppingCartIcon />
+                </IconButton>
+              </div>
             </div>
-            <div className="d-flex justify-content-between ">{product.category}
-            <IconButton color="black" aria-label="add to shopping cart" onClick={()=>dispatch(addToBasket())}>
-        <AddShoppingCartIcon />
-      </IconButton>
-      </div>
-          </div>
-        ))}
-      </div>
-     
-    </Card>
+          ))}
+        </div>
+      </Card>
     </Container>
   );
 };
